c300f4e7e2077bf5b743f9352d0bd262bc88ed98
SaraSA1977 <sanchezs.sara@uces.edu.co>
2025-07-31 23:20:16 -0500

    README actualizado

--- arritmia.py
+++ arritmia.py
@@ -1,14 +1,14 @@
-M=int(input("¡cuantos valores de frecuencia cardiaca va a ingresar?"))
+M = int(input("¿Cuántos valores de frecuencia cardiaca va a ingresar? "))
 fuera_de_rango = 0
 contador = 0
-while contador < M:
-     frecuencia = int(input("Ingrese frecuencia cardíaca (ppm): "))
-     if frecuencia < 60 or frecuencia > 100:
-        fuera_de_rango = fuera_de_rango + 1
 
-        contador = contador + 1
+while contador < M:
+    frecuencia = int(input("Ingrese frecuencia cardíaca (ppm): "))
+    if frecuencia < 60 or frecuencia > 100:
+        fuera_de_rango += 1
+    contador += 1 
 
 if fuera_de_rango > 3:
-    print("potencial arritmia")
+    print("POTENCIAL ARRITMIA")
 else:
-    print("ritmico")
+    print("Rítmico")
